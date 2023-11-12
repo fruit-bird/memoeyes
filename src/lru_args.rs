8b53c80d4568cb6bcedd23132bf5e058abc0f82b
fruit-bird <github.ru3w9@slmail.me>
2023-11-12 17:33:07 +0100

    made `LruArgs` more AST struct-like

--- src/lru_args.rs
+++ src/lru_args.rs
@@ -1,5 +1,4 @@
 use proc_macro2::Ident;
-use std::num::NonZeroUsize;
 use syn::{
     parse::{Parse, ParseStream},
     Error, LitInt, Result, Token,
@@ -10,14 +9,14 @@ const LRU_ARGS_IDENT: &str = "max";
 pub struct LruArgs {
     pub max_ident: Ident,
     pub eq_token: Token![=],
-    pub cap: NonZeroUsize,
+    pub cap: LitInt,
 }
 
 impl Parse for LruArgs {
     fn parse(input: ParseStream) -> Result<Self> {
         let max_ident = input.parse::<Ident>()?;
         let eq_token = input.parse()?;
-        let cap = input.parse::<LitInt>()?.base10_parse::<NonZeroUsize>()?;
+        let cap = input.parse()?;
 
         if max_ident != LRU_ARGS_IDENT {
             return Err(Error::new(
